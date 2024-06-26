6afad4ae17fe022b752c331745cc0a5555747271
Oscar <oscab05@outlook.com>
2024-06-26 12:28:50 -0300

    icono interactivo

--- action.js
+++ action.js
@@ -1,18 +1,38 @@
 let aside = document.querySelector(".aside")
-let botonMenu= document.querySelector(".icono")
+let botones = document.querySelectorAll(".icono")
+let botonMenu = document.querySelector(".menu")
 let containerGeneral = document.querySelector(".prueba")
-let asideLinks= document.querySelectorAll(".indice__link")
+let asideLinks = document.querySelectorAll(".indice__link")
 let backgroundAside = document.querySelector(".general__container__info")
+let x = document.querySelector(".x")
+let menuAbierto = false;
 
-botonMenu.addEventListener("click", () => {
-    aside.classList.toggle("aside__responsive")
-    containerGeneral.classList.toggle("contenedor__responsive")
-    backgroundAside.classList.toggle("background__aside")
-    asideLinks.forEach((link) => {
-        link.addEventListener("click", () => {
-            aside.classList.remove("aside__responsive")
-            containerGeneral.classList.remove("contenedor__responsive")
-            backgroundAside.classList.remove("background__aside")
-        })
+
+botones.forEach((boton) => {
+    boton.addEventListener("click", () => {
+        aside.classList.toggle("aside__responsive")
+        containerGeneral.classList.toggle("contenedor__responsive")
+        backgroundAside.classList.toggle("background__aside")
+
+        if (menuAbierto) {
+            x.style.display = "none";
+            botonMenu.style.display = "block";
+        } else {
+            x.style.display = "block";
+            x.style.borderRadius = "50px"
+            botonMenu.style.display = "none";
+        }
+
+        menuAbierto = !menuAbierto;
+    })
+})
+asideLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        aside.classList.remove("aside__responsive")
+        containerGeneral.classList.remove("contenedor__responsive")
+        backgroundAside.classList.remove("background__aside")
+        menuAbierto= false;
+        x.style.display = "none";
+        botonMenu.style.display = "block";
     })
 })
