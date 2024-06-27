6b2e5a3b2922fa640d40fe9dcb778b0d04762290
Oscar <oscab05@outlook.com>
2024-06-26 22:25:29 -0300

    mejora menu

--- action.js
+++ action.js
@@ -1,9 +1,7 @@
 let aside = document.querySelector(".aside")
 let botones = document.querySelectorAll(".icono")
 let botonMenu = document.querySelector(".menu")
-let containerGeneral = document.querySelector(".prueba")
 let asideLinks = document.querySelectorAll(".indice__link")
-let backgroundAside = document.querySelector(".general__container__info")
 let x = document.querySelector(".x")
 let menuAbierto = false;
 
@@ -11,8 +9,6 @@ let menuAbierto = false;
 botones.forEach((boton) => {
     boton.addEventListener("click", () => {
         aside.classList.toggle("aside__responsive")
-        containerGeneral.classList.toggle("contenedor__responsive")
-        backgroundAside.classList.toggle("background__aside")
 
         if (menuAbierto) {
             x.style.display = "none";
@@ -29,8 +25,6 @@ botones.forEach((boton) => {
 asideLinks.forEach((link) => {
     link.addEventListener("click", () => {
         aside.classList.remove("aside__responsive")
-        containerGeneral.classList.remove("contenedor__responsive")
-        backgroundAside.classList.remove("background__aside")
         menuAbierto= false;
         x.style.display = "none";
         botonMenu.style.display = "block";
